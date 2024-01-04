a1e1ab7cf8e08b86cefe9f8ac87ee824e313a5ea
chiefkarim <krimotop2016@gmail.com>
2024-01-04 11:13:23 +0100

    add mobile responsiveness to homepage

--- big-mama/src/components/DemoAccounts.tsx
+++ big-mama/src/components/DemoAccounts.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { DemoAccountLogIn } from "@/lib/actions";
 import { FormEvent, useState, useTransition } from "react";
-import {Button} from '@nextui-org/button'; 
+import { Button } from "@nextui-org/button";
+import { Select, SelectItem } from "@nextui-org/react";
 
 export default function DemoAccounts() {
   const [isPending, startTransiton] = useTransition();
-  const [account, setAccount] = useState("Jory Quispe");
+  const [account, setAccount] = useState("");
   const [error, setError] = useState("");
   function handleChnage(e: any) {
     setAccount(() => {
@@ -14,42 +15,55 @@ export default function DemoAccounts() {
   }
   async function handelForm(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    setError("");
+    if (account === "") {
+      return setError("Please, chose one of the available accounts");
+    }
     startTransiton(async () => {
-      setError("");
       const result = await DemoAccountLogIn({ account });
       const { error } = JSON.parse(result);
       if (error) {
         setError(error.message);
-       
       }
     });
   }
   return (
     <form
       onSubmit={handelForm}
-      className=" p-4 shadow-md bg-white rounded text-base "
+      className="flex flex-wrap w-[250px] justify-center gap-4 p-4 shadow-md bg-white rounded text-base "
     >
-      <label className=" text-gray-800 p-1">select demo account</label>
-      <select
+      <Select
         id="account"
+        label="select demo account"
         name="account"
+        size="sm"
         onChange={handleChnage}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="  text-gray-900 w-full "
       >
-        <option value="Jory Quispe">Jory Quispe</option>
-        <option value="Quinn Elton">Quinn Elton</option>
-        <option value="Emil Joyce">Emil Joyce</option>
-      </select>
+        <SelectItem value="Jory Quispe" key="Jory Quispe">
+          Jory Quispe
+        </SelectItem>
+        <SelectItem value="Quinn Elton" key="Quinn Elton">
+          Quinn Elton
+        </SelectItem>
+        <SelectItem value="Emil Joyce" key="Emil Joyce">
+          Emil Joyce
+        </SelectItem>
+      </Select>
       <div className="flex justify-center mt-3 m-2 ">
-       <Button type="submit" className=" bg-blue-500 text-white "
->Try it now</Button>
+        <Button
+          type="submit"
+          className=" bg-blue-500 text-white "
+          isLoading={isPending}
+        >
+          Try it now
+        </Button>
       </div>
-      <span className=" text-gray-800 text-sm"> {error ?? ""}</span>
-      <div className={isPending ? "block" : `hidden`}>
-        <div className="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">
-          loading...
-        </div>
-      </div>
+
+      <span className=" text-gray-800 text-sm text-center">
+        {" "}
+        {error ?? null}
+      </span>
     </form>
   );
 }
